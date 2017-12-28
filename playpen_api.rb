2e8204eda15f5d556d525d7eff39addac1f93363
Robby Ranshous <rranshous@gmail.com>
2017-12-28 16:51:53 -0500

    playpen can add new robogs and get their status

--- playpen_api.rb
+++ playpen_api.rb
@@ -1,5 +1,6 @@
 # GOAL: manage robogachis running on docker swarm
 
+require 'net/http'
 require 'docker-swarm-api'
 require_relative 'simpleagent'
 
@@ -8,51 +9,92 @@ set :http_port, (ENV['HTTP_PORT'] || 80).to_i
 set :state_root, ENV['STATE_DIR'] || './state'
 set :swarm_connection_string, ENV['DOCKER_HOST']
 set :image_name, ENV['IMAGE_NAME'] || 'rranshous/robogachi'
+set :cluster_host, ENV['CLUSTER_HOST'] || 'localhost'
 
 state_field :added_names, []
 state_field :removed_names, []
 
+def log msg
+  STDOUT.print "#{msg}\n"
+  STDOUT.flush
+end
+
+log "it's playpen time!"
+
 action 'add_new' do |state, opts|
+  log 'adding new'
   agent_name = generate_random_name
-  puts "agent name: #{agent_name}"
+  log "agent name: #{agent_name}"
   container_name = agent_name
-  docker_host = 'unix:///var/run/docker.sock'
   replicas = 1
-  master_connection = Docker::Swarm::Connection.new(docker_host)
-  swarm = Docker::Swarm::Swarm.find(master_connection)
-  puts "swarm: #{swarm}"
+  log "swarm: #{swarm}"
   # TODO: mount volume from host
   service_def = {
     "Name"=>container_name,
     "TaskTemplate" =>
     {"ContainerSpec" =>
-     {"Networks" => [], "Image" => Config.get(:image_name), "Command" => [agent_name], "Mounts" => [], "User" => "root"},
-       "Env" => [],
-       "Placement" => {},
-       "RestartPolicy" => {"Condition"=>"on-failure", "Delay"=>1, "MaxAttempts"=>3}},
+     {"Networks" => [],
+      "Image" => Config.get(:image_name),
+      "Args" => [agent_name],
+      "Mounts" => [],
+      "User" => "root"},
+      "Env" => [],
+      "Placement" => {},
+      "RestartPolicy" => {"Condition"=>"on-failure",
+                          "Delay"=>1,
+                          "MaxAttempts"=>3}},
     "Mode"=>{"Replicated" => {"Replicas" => replicas}},
-    "UpdateConfig" => {"Delay" => 2, "Parallelism" => 2, "FailureAction" => "pause"},
+    "UpdateConfig" => {"Delay" => 2,
+                       "Parallelism" => 2,
+                       "FailureAction" => "pause"},
     "EndpointSpec"=>
-     {"Ports" => [{"Protocol"=>"tcp", "TargetPort" => 80}]},
+     {"Ports" => [{"Protocol"=>"tcp",
+                   "TargetPort" => 80}]},
   }
-  puts "creating service: #{service_def}"
+  log "creating service: #{service_def}"
   r = swarm.create_service(service_def)
-  puts "created service: #{r}"
+  log "created service: #{r}"
   # TODO: block until service starts?
   state.added_names << agent_name
   { name: agent_name }
 end
 
 report 'status' do |state|
-  { robogachi_names: names }
+  log 'getting status'
+  { robogachi_names: names(state) }
 end
 
-report 'get_status' do |state, opts|
-  # return the response from the robogachi /action/status
+report 'status_of' do |state, opts|
+  params = opts[:request].query
+  name = params["name"]
+  uri = URI(url_for(name))
+  log "getting status of: #{uri.to_s}"
+  Net::HTTP.start(uri.host, uri.port) do |http|
+    JSON.parse(http.request_get('/action/status').read_body)
+  end
 end
 
-report 'get_about' do |state, opts|
-  # return the repsonse from the robogachi /action/about
+action 'forward' do |state, opts|
+  params = opts[:request].query
+  name = params["name"]
+  url = url_for(name)
+  opts[:response]['Location'] = url.to_s
+  { url: url }
+end
+
+def url_for name
+  service = swarm.find_service_by_name(name)
+  port = service.hash["Endpoint"]["Ports"].map do |port_def|
+    port_def["PublishedPort"]
+  end.first
+  forward_url = "http://#{Config.get(:cluster_host)}:#{port}"
+  forward_url
+end
+
+def swarm
+  docker_host = 'unix:///var/run/docker.sock'
+  @master_connection ||= Docker::Swarm::Connection.new(docker_host)
+  Docker::Swarm::Swarm.find(@master_connection)
 end
 
 def names state
@@ -65,4 +107,4 @@ def generate_random_name
   "robogachi-#{random_words.join('-')}"
 end
 
-puts "loaded"
+log 'done loading'
